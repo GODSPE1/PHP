f5d5c965d24fe8f3893a8efeb8981483b6c54645
GODSPE1 <godspy18@gmail.com>
2026-02-27 04:37:10 +0100

    Initial commit: Add base PHP application files and configuration

---
+++ index.php
@@ -0,0 +1,41 @@
+// First PHP code and hello world
+Hello World
+<h1>PHP Crash Course</h1>
+
+<?php
+    echo "Hello World";
+?>
+
+<?php
+    echo "Hello World Again";
+?>
+
+
+// line comment
+<?php
+    echo "Hello World Again Again";
+?>
+
+<?php
+    echo "hat the hell";
+    echo "jcbskjb"
+?>
+
+// Let's see how to print multiple things in PHP
+<?php
+    echo "WOWO" . 456 . "HUUGH";
+    ?>
+<?php
+    echo "Hello ", 123 , " World";
+    ?>
+
+
+print 123, 123, 123;
+
+
+print 123 . 123 . 123;
+
+// print 123, 123, 123; // This will cause an error because print does not support multiple arguments.
+// but it printed
+
+print_r([1,3,5,7,9]);
